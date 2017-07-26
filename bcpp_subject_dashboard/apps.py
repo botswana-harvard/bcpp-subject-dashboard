68d0e758b6036119b2045edcdf3c3d3caec0be56
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-07-26 15:15:42 +0200

    pep8 and updating navbar_item_selected to bcpp_subject_dashboard

--- bcpp_subject_dashboard/apps.py
+++ bcpp_subject_dashboard/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig as DjangoApponfig
 
 class AppConfig(DjangoApponfig):
     name = 'bcpp_subject_dashboard'
+    listboard_template_name = 'bcpp_subject/listboard.html'
     listboard_url_name = 'bcpp_subject_dashboard:listboard_url'
     anonymous_listboard_url_name = 'bcpp_subject_dashboard:anonymous_listboard_url'
     dashboard_url_name = 'bcpp_subject_dashboard:dashboard_url'
