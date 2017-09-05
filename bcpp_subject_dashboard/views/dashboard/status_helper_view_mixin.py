516d94650f6d81648ffb6f813475cb6e2844888e
erikvw <ew2789@gmail.com>
2017-09-05 08:30:18 +0200

    use status db helper

--- bcpp_subject_dashboard/views/dashboard/status_helper_view_mixin.py
+++ bcpp_subject_dashboard/views/dashboard/status_helper_view_mixin.py
@@ -1,11 +1,10 @@
-# from bcpp_status.status_db_helper import StatusDbHelper
+from bcpp_status.status_db_helper import StatusDbHelper
 from edc_constants.constants import UNK
-from bcpp_status.status_helper import StatusHelper
 
 
 class StatusHelperViewMixin:
 
-    status_helper_cls = StatusHelper
+    status_helper_cls = StatusDbHelper
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
