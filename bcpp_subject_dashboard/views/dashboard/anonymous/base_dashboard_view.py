95f26407f0614d1c951d679aa504dc6cdaa1ad1d
erikvw <ew2789@gmail.com>
2017-11-08 10:10:59 +0200

    update imports

--- bcpp_subject_dashboard/views/dashboard/anonymous/base_dashboard_view.py
+++ bcpp_subject_dashboard/views/dashboard/anonymous/base_dashboard_view.py
@@ -1,5 +1,5 @@
 from bcpp_referral.referral_view_mixin import ReferralViewMixin
-from edc_dashboard.view_mixins import ShowHideViewMixin, SubjectIdentifierViewMixin
+from edc_subject_dashboard.view_mixins import SubjectVisitViewMixin, ShowHideViewMixin, SubjectIdentifierViewMixin
 from edc_metadata.view_mixins import MetaDataViewMixin
 from household_dashboard.view_mixins import HouseholdLogEntryViewMixin
 from household_dashboard.view_mixins import HouseholdViewMixin, HouseholdStructureViewMixin
@@ -8,7 +8,6 @@ from survey.view_mixins import SurveyViewMixin
 
 from ..appointment_view_mixin import AppointmentViewMixin
 from ..consent_view_mixin import ConsentViewMixin
-from ..subject_visit_view_mixin import SubjectVisitViewMixin
 from ..visit_schedule_view_mixin import VisitScheduleViewMixin
 from ....views.dashboard.status_helper_view_mixin import StatusHelperViewMixin
 
